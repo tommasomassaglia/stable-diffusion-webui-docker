9983fd94b447aa90a2356678c2c21963e3c0df1a
tommasomassaglia <tommaso.massaglia@alkemy.com>
2024-02-27 13:28:53 +0000

    Added styles.

--- services/download/download.sh
+++ services/download/download.sh
@@ -15,12 +15,16 @@ mkdir -vp /data/.cache \
   /data/models/Lora \
   /data/models/ControlNet
 
+cp /data/styles.csv /data/config/auto/styles.csv
+
 echo "Downloading, this might take a while..."
 
 aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data --continue
 
 # CN_DIR=/data/config/auto/extensions/sd-webui-controlnet
 # git -C "$CN_DIR" pull || git clone https://github.com/Mikubill/sd-webui-controlnet "$CN_DIR"
+
 PL_DIR=/data/config/auto/extensions/sd-webui-api-payload-display
 git -C "$PL_DIR" pull || git clone https://github.com/huchenlei/sd-webui-api-payload-display "$PL_DIR"
+
 aria2c -x 10 --disable-ipv6 --input-file /docker/controlnet_links.txt --dir /data/models/ControlNet --continue
